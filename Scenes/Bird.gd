57b17d3a2080710e09417c043520f1349cb1e16d
Miguel Sánchez <mangelsr25@gmail.com>
2023-11-10 23:08:53 -0500

    [UPD] Free condition, evaluate property pipe of Pipe scene

--- Scenes/Bird.gd
+++ Scenes/Bird.gd
@@ -5,10 +5,10 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.pressed:
 			self.set_linear_velocity(Vector2.ZERO)
-			self.apply_impulse(Vector2(0, -350))
+			self.apply_impulse(Vector2(0, -250))
 			# self.set_angular_velocity(-3)
 			$AnimatedSprite2D.play("fly")
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Floor" or body.name == "Pipes":
+	if body.name == "Floor" or "pipe" in body:
 		self.queue_free()
