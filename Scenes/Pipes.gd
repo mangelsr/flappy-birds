ed7ebaff43df81d36225c63e4ea400290d337e6f
Miguel Sánchez <mangelsr25@gmail.com>
2023-11-10 23:07:11 -0500

    [UPD] Pipes to use variables instead of hardcoded values

--- Scenes/Pipes.gd
+++ Scenes/Pipes.gd
@@ -1,5 +1,8 @@
 extends TileMap
 
+var pipe
+var initial_position = 10
+var speed = 60
 
 func _ready():
 	var randRangeTop = randi_range(1, 15)
@@ -7,22 +10,22 @@ func _ready():
 	# Set Top Pipe
 	# Build Pipe tube
 	for i in range(randRangeTop):
-		set_cell(0, Vector2i(6, i), 0, Vector2i(0, 2), 0)
+		set_cell(0, Vector2i(initial_position, i), 0, Vector2i(0, 2), 0)
 
 	# Build Pipe mouth
-	set_cell(0, Vector2i(6, randRangeTop), 0, Vector2i(0, 3), 0)
-	set_cell(0, Vector2i(6, randRangeTop + 1), 0, Vector2i(0, 4), 0)
+	set_cell(0, Vector2i(initial_position, randRangeTop), 0, Vector2i(0, 3), 0)
+	set_cell(0, Vector2i(initial_position, randRangeTop + 1), 0, Vector2i(0, 4), 0)
 
 
 	# Set Bottom Pipe	
 	# Build Pipe mouth
-	set_cell(0, Vector2i(6, randRangeTop + 7), 0, Vector2i(0, 0), 0)
-	set_cell(0, Vector2i(6, randRangeTop + 8), 0, Vector2i(0, 1), 0)	
+	set_cell(0, Vector2i(initial_position, randRangeTop + 7), 0, Vector2i(0, 0), 0)
+	set_cell(0, Vector2i(initial_position, randRangeTop + 8), 0, Vector2i(0, 1), 0)	
 
 	# Build Pipe tube
 	for i in 15:
-			set_cell(0, Vector2i(6, randRangeTop + 8 + i), 0, Vector2i(0, 2), 0)
+			set_cell(0, Vector2i(initial_position, randRangeTop + 8 + i), 0, Vector2i(0, 2), 0)
 
 func _process(delta):
-	self.position.x -= 60 * delta
+	self.position.x -= speed * delta
 
